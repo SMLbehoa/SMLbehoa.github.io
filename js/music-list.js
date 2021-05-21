417b667cf3a8925217a3c9a4e5c579b018456341
SMLbehoa <84579309+SMLbehoa@users.noreply.github.com>
2021-05-21 20:50:44 +0700

    upload lần đầu

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+
+
+let allMusic = [
+
+    {
+    name: "Vách Ngọc Ngà (New Version)",
+    artist: "Anh Rồng, Phan Ann",
+    img: "behoa-1",
+    src: "behoa-1"
+  },
+  {
+  	name: "Waiting For Love",
+    artist: "Avicii",
+    img: "behoa-2",
+    src: "behoa-2"
+  },
+  {
+  	name: "The Nights",
+    artist: "Avicii",
+    img: "behoa-3",
+    src: "behoa-3"
+  },
+  {
+  	name: "Chỉ Là Không Cùng Nhau",
+    artist: "Tăng Phúc, Trương Thảo Nhi",
+    img: "behoa-4",
+    src: "behoa-4"
+  },
+  {
+  	name: "Cô Độc Vương",
+    artist: "Thiên Tú",
+    img: "behoa-5",
+    src: "behoa-5"
+  },
+
+];
